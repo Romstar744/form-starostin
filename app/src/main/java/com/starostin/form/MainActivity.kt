6bfcdfd7e0a05970413ed6586eb62e88df2aedfb
Romstar744 <romstar742@gmail.com>
2024-02-12 21:41:42 +0300

    1.0

---
+++ app/src/main/java/com/starostin/form/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.starostin.form
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.RadioGroup
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var no2free: LinearLayout
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        no2free = findViewById(R.id.no2free)
+        val radioGroup = findViewById<RadioGroup>(R.id.radioGroup)
+        radioGroup.setOnCheckedChangeListener { _, checkedId ->
+            when (checkedId) {
+                R.id.free -> {
+                    no2free.visibility = View.GONE
+                }
+                R.id.nofreeee -> {
+                    no2free.visibility = View.VISIBLE
+                }
+            }
+        }
+    }
+}
